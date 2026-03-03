0488ab62996ce2b28a6bff8abebefdae98dca8f7
rajyadav2020 <rajyadavv176@gmail.com>
2026-03-03 21:40:35 +0530

    api response and api error handling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import dns from "node:dns/promises";
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 // Load environment variables
 dotenv.config({ path: './.env' });
@@ -10,8 +11,10 @@ dotenv.config({ path: './.env' });
 connectDB()
     .then(() => {
         console.log("Database connection successful!");
-        // Start your express server here (app.listen)
+        app.listen(process.env.PORT || 8000 , ()=>{
+            console.log(`server is running at PORT : ${process.env.PORT}`)
+        })
     })
     .catch((err) => {
         console.log("Database connection failed!", err);
-    });
+});
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,24 @@
+const asynchandler = (reqhandler) =>{
+    (req,res,next) =>{
+        Promise.resolve(reqhandler(req,res,next)).catch
+        ((err)=>next(err) )
+    }
+
+}
+
+
+
+export {asynchandler}
+
+
+// const asynchandler = (fn) => async (req,res,next)=>{
+//     try{
+//         await fn(req,res,next)
+
+//     }catch(error){
+//         res.status(error.code || 500).json({
+//             success : false,
+//             message: error.message
+//         })
+//     }
+// }
