7cdae9f79ebc7cde70f740eb8988eec7ccd88af2
rajyadav2020 <rajyadavv176@gmail.com>
2026-03-05 21:15:16 +0530

    uploading the files in db

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_CLOUD_APIKEY, 
+  api_secret: process.env.CLOUDINARY_CLOUD_SECRET
+});
+
+const uploadoncloudinary = async (localfilepath)=>{
+    try{
+        if(!localfilepath) return null;
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localfilepath,{
+            resource_type: "auto"
+        })
+        //fille has been uploaded successfully
+
+        console.log("file is uploaded on cloudinary",
+            response.url
+        );
+        return response
+
+    }catch(error){
+        fs.unlinkSync(localfilepath) //removes the localy saved temporary file as the upload operation got failes 
+    }
+
+}
+
+export {uploadoncloudinary}
