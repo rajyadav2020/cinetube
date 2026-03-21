36af14f4430dd1488ab6671b292ffded885fd036
rajyadav2020 <rajyadavv176@gmail.com>
2026-03-22 01:03:24 +0530

    implemented and learned aggregated pipelines in mongodb

--- Readme.md
+++
@@ -1,3 +0,0 @@
--->backend code for my this cinetube app
-
-//database 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -370,7 +370,82 @@ const updateUserCoverImage = asynchandler(async(req,res)=>{
     )
 })
 
+const getUserChannelProfile = asynchandler(async(req,res)=>{
+    const {username} = req.params
 
+    if(!username?.trim()){
+        throw new Apierror(400,"username is missing")
+    }
+
+   const channel = await User.aggregate([
+    {
+        $match:{
+            username:username?.toLowerCase()
+        }
+    },
+    {
+        $lookup:{
+            from:"subscriptions",
+            localField:"_id",
+            foreignField:"channel",
+            as:"subscribers"
+        }
+    },
+    {
+        $lookup:{
+            from:"subscriptions",
+            localField:"_id",
+            foreignField:"subscriber",
+            as:"subscribedTo"
+        }
+    },
+    {
+        $addFields:{
+            subscribersCount:{
+                $size:"$subscribers"
+            },
+            subscribedToCount:{
+                $size:"$subscribedTo"
+            },
+            isSubscribed:{
+                $cond:{
+                    if:{
+                        $in:[
+                            req.user?._id,
+                            "$subscribers.subscriber"
+                        ]
+                    },
+                    then:true,
+                    else:false
+                }
+            }
+        }
+    },
+    {
+        $project:{
+            fullname:1,
+            username:1,
+            avatar:1,
+            coverImage:1,
+            subscribersCount:1,
+            subscribedToCount:1,
+            isSubscribed:1
+        }
+    }
+   ])
+
+   if(!channel){
+    throw new Apierror(404, "Channel not found")
+   }
+
+   return res.status(200).json(
+    new Apiresponse(
+        200,
+        channel,
+        "Channel fetched successfully"
+    )
+   )
+})
 
 export {
     registerUser,
